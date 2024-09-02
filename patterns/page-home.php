276ef6bc5bf8a635f84a3564b07ed247d01b0e0f
Tim <github@timgreen.ws>
2024-09-02 14:56:15 +1000

    added page home pattern

---
+++ patterns/page-home.php
@@ -0,0 +1,10 @@
+<?php 
+/**
+ * Title: Home page
+ * Slug: littleboxes/page-home
+ * Categories: littleboxes-pages
+ */
+?>
+<!-- wp:pattern {"slug":"littleboxes/hero-default"} /-->
+<!-- wp:pattern {"slug":"littleboxes/projects"} /-->
+<!-- wp:pattern {"slug":"littleboxes/recent-posts"} /-->
